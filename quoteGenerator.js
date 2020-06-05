e5134b5852d8942b2a0263cf380b20ddd9391301
Natacha <natacha.har@onja.org>
2020-06-05 10:42:36 +0300

    Fixed the array

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,15 +1,34 @@
 
-console.log('it works!');
+let quotes1 = [
+    "Do not give up",
+    "keep trying",
+    "never stop",
+    "you will get it"];
+let quotes2 = [
+    "Try everything",
+    "but before you try",
+    "think because it will",
+    "return back to you"];
+let quotes3 = [
+    "You will be fine",
+    " don't worry",
+    "just go there",
+    " to much"];
+    
 
-const quotes = [
-    "Don't give up, keep trying, never stop and you will get it.",
-    " Don't give up, keep trying, never stop and you will get it.", 
-    " Try everything but before you try, think because it will return back to you.", 
-    "You will be fine don't worry to much, just go there"
-];
+ function randomQuote(quotes1) {
+    return quotes1[Math.floor(Math.random() * quotes1.length)];
+ }
+ console.log(randomQuote(quotes1));
 
-function randomQuote(quotes) {
-   return quotes[Math.floor(Math.random() * quotes.length)];
-}
+function randomQuote(quotes2) {
+    return quotes2[Math.floor(Math.random() * quotes2.length)];
+ }
+ console.log(randomQuote(quotes2));
+ 
+ function randomQuote(quotes3) {
+    return quotes3[Math.floor(Math.random() * quotes3.length)];
+ }
+ console.log(randomQuote(quotes3))
 
-console.log(randomQuote(quotes));
+let randomQuotes = quotes1 + quotes2 + quotes3;
