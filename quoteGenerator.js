1d6f0534ce1042de128e8d8e943f469f6dac9ac4
Natacha <natacha.har@onja.org>
2020-06-09 16:33:08 +0300

    generated random quote and show the generated quote

--- quoteGenerator.js
+++ quoteGenerator.js
@@ -1,19 +1,19 @@
 
 let quotes1 = [
-    "do not give up",
-    "keep trying",
-    "never stop",
-    "you will get it"];
+    "do not give up,",
+    "keep trying,",
+    "never stop,",
+    "you will get it,"];
 let quotes2 = [
-    "try everything",
-    "but before you try",
-    "think because it will",
+    "try everything,",
+    "but before you try,",
+    "think because it will,",
     "return back to you"];
 let quotes3 = [
-    "you will be fine",
-    " don't worry",
-    "just go there",
-    " to much"];
+    "you will be fine,",
+    " don't worry,",
+    "just go there,",
+    " to much,"];
 
 let randomIndex1 = Math.floor(Math.random() * quotes1.length);
 let randomIndex2 = Math.floor(Math.random() * quotes2.length);
@@ -24,6 +24,19 @@ let string2 = quotes1[randomIndex2];
 let string3 = quotes1[randomIndex3];
 
 let quote = `${string1} ${string2} ${string3}`;
+console.log(quote);
+
+const generatedQuote = () => {
+    let randomIndex1 = Math.floor(Math.random() * quotes1.length);
+    let randomIndex2 = Math.floor(Math.random() * quotes2.length);
+    let randomIndex3 = Math.floor(Math.random() * quotes3.length);
+    let string1 = quotes1[randomIndex1];
+    let string2 = quotes1[randomIndex2];
+    let string3 = quotes1[randomIndex3];
+    let quote = `${string1} ${string2} ${string3}`;
+    return quote;
+}
+
 
 /*  
 function randomQuote(quotes1) {
@@ -47,17 +60,21 @@ console.log(randomQuotes);
 */
 
 //1. use prompt to ask the user how many times we want to generate a random quote
+const quotesNumber = Number(prompt("How many quotes do you want to generate (1 to 5"));
 //2. check if the prompt give us something between 1 and 5
+if (quotesNumber >= 1  && quotesNumber <= 5) {
 //3. we're going to use a loop somewhere, and it's going to last the number of times that the user entered in the prompt!
+for(let i = 0; i < quotesNumber; i++) { 
 //4. refactor our code into a function, and put the function call inside the loop
 //4.1 generate random numbers
 //4.2 show the generated quotes
+let quote = generatedQuote();
+		console.log(quote);
+	}
 
-let quotes = prompt("How many times we want to generate a random quote:");
-    if (quotes >= 1 && quotes <= 5) {
-    }
-const randomQuotes = (randomIndex1, randomIndex2, randomIndex3) => {
-    const quotes = randomIndex1 + randomIndex2 + randomIndex3;
-    return quotes;
+    let quote = generatedQuote();
+} else {
+    console.error('We need a number between 1 and 5');
 }
-console.log(quote);
+
+
